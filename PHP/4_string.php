63ee01c9ede7565521baf3fda07addee0a08156b
Lukomska <ola.lukomska11@wp.pl>
2020-10-17 14:53:39 +0200

    1

--- PHP/4_string.php
+++ PHP/4_string.php
@@ -11,9 +11,31 @@ $name = 'jANUsZ';
 echo "$name<br>";
 
 //zamiana na małe litery
+$name = strtolower($name);
+echo $name;
 
 //zamiana na duże litery 
+echo '<br>';
+$name = strtoupper($name);
+echo $name;
 
-//zamiana pierwszje litery na dużą a pozostałych na małe 
+//zamiana pierwszej litery na dużą a pozostałych na małe 
+$name = strtolower($name);
+echo '<br>';
+$name = ucfirst(strtolower($name));
+echo $name;
+echo '<br>';
+
+//w obu wyrazach duże litery
+$name = "jANUsZ kowALski";
+echo ucwords(strtolower($name)),'<br>';
+
+
+// LOREM IPSUM
+$lorem = <<<LOREM
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit reiciendis quaerat nisi qui, temporibus excepturi corrupti molestias harum aspernatur quis aliquam libero similique aliquid porro veritatis neque in. Earum, dolores.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit reiciendis quaerat nisi qui, temporibus excepturi corrupti molestias harum aspernatur quis aliquam libero similique aliquid porro veritatis neque in. Earum, dolores.<br>
+LOREM;
+
+echo $lorem;
 
 ?>

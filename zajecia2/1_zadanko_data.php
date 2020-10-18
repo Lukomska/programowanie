53fd560cee96da48c15c8535269ca7c3455ea617
Lukomska <ola.lukomska11@wp.pl>
2020-10-18 09:44:56 +0200

    1

--- zajecia2/1_zadanko_data.php
+++ zajecia2/1_zadanko_data.php
@@ -12,20 +12,21 @@
 
 //operatory logiczne AND $$, OR ||
 
-If (!empty($_GET['name']) && !empty(GET['surname'])) {
-    echo 'imie wprowadzone';
-}else{
-    echo 'Wypełnij wszystkie dane';
-}
+  //Sprawdż czy użytkownik wypełnił wszystkie dane , jeśli tak to wyświetl mu heredoc, w przeciwnym razie przekieruj użytkownika do formularza 
 
-$data = <<<T
+
+If (!empty($_GET['name']) && !empty($_GET['surname']) && !empty($_GET['pass']) && !empty($_GET['zipcode']) && !empty($_GET['zipcode2'])) {
+    echo 'Dane wprowadzone';
+    echo <<<T
     Imię i nazwisko: $_GET[name] $_GET[surname]<br>
     Hasło: $_GET[pass]<br>
     Kod Pocztowy: $_GET[zipcode] - $_GET[zipcode2]
-    
     T;
-
-    //Sprawdż czy użytkownik wypełnił wszystkie dane , jeśli tak to wyświetl mu heredoc, w przeciwnym razie przekieruj użytkownika do formularza 
+}else{
+    header('Location: http://localhost/git/zajecia2/1_zadanko.php');
+}
+// po nie wypełnieniu danych. przekierowuje użytkownika do formularza, dane w formularzu mają być wypełnione (oprocz hasła)
+// wykorzystuje skrypt w js (historia)
 
  ?>   
     

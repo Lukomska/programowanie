b222deee520721d8d09171c314c396667665d2c1
Lukomska <ola.lukomska11@wp.pl>
2020-10-19 18:19:59 +0200

    homework

--- zajecia2/homework/homework.php
+++ zajecia2/homework/homework.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./homework.css">
     <title>Document</title>
 </head>
 
@@ -12,7 +13,7 @@
     <form name="form1" action="./homework_data.php" method="post">
         <input type="text" size=2 name="a" placeholder="cm"><br><br>
         <input type="text" size=2 name="b" placeholder="cm"><br><br>
-        <input type="radio" name="po" value="p">Pole<br><br>
+        <input type="radio" name="po" value="p" checked>Pole<br><br>
         <input type="radio" name="po" value="o">Obwód<br><br>
         <input type="submit" name="oblicz" value="Oblicz"><br><br>
     </form>

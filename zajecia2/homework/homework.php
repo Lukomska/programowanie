28acb7202a01a0e1ee38f7485f96bbc9a8839efa
Lukomska <ola.lukomska11@wp.pl>
2020-10-19 19:23:07 +0200

    qwerty

--- zajecia2/homework/homework.php
+++ zajecia2/homework/homework.php
@@ -11,11 +11,13 @@
 <body>
     <h4>PODAJ DWIE DŁUGOŚCI</h4>
     <form name="form1" action="./homework_data.php" method="post">
+
         <input type="text" size=2 name="a" placeholder="cm"><br><br>
         <input type="text" size=2 name="b" placeholder="cm"><br><br>
         <input type="radio" name="po" value="p" checked>Pole<br><br>
         <input type="radio" name="po" value="o">Obwód<br><br>
         <input type="submit" name="oblicz" value="Oblicz"><br><br>
+
     </form>
 </body>
 

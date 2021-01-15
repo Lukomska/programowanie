165164879b1869d9c727f59f279256ae646124ca
Lukomska <ola.lukomska11@wp.pl>
2021-01-15 19:16:53 +0100

    1

---
+++ zajęcia6/BAZY_DANYCH/1_6_customers_usuwanie/site/miasto.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista miast</h1>
+    <?php
+
+require_once("../scripts/connect.php");
+
+$sql = "SELECT cities.city,states.state FROM `cities` INNER JOIN states ON cities.states_id=states.id ORDER BY state";
+
+$result = $connect->query($sql);
+
+echo "<table>";
+echo "<tr>
+<th>Miasto</th>
+<th>Województwo</th>
+</tr>";
+
+while ($row = $result->fetch_assoc()) {
+
+   echo <<<ROW
+   <tr>
+    <td>$row[city]</td> 
+    <td>$row[state]</td>
+   </tr>
+   ROW;
+}
+echo "</table>";
+$connect->close(); //zamknięcie połączenia z bazą 
+?>
+</body>
+</html>
---
+++ zajęcia6/BAZY_DANYCH/1_6_customers_usuwanie/strona_glowna.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Strona Główna</h1>
+    <a href="./site/klient.php">Lista Klientów<br></a>
+    <a href="./site/miasto.php">Lista Miast<br></a>
+    <a href="./site/wojewodztwo.php">Lista Województw<br></a>
+    <a href="./site/statystyki.php">Statystyki<br></a>
+    <a href="./site/klienci_usuwanie.php">Klienci - usuwanie<br></a>
+</body>
+</html>

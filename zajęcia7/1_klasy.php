feb508bbb4124f6e9601d4a3375a41522bff60b3
Lukomska <ola.lukomska11@wp.pl>
2021-02-10 10:47:27 +0100

    class

--- zajęcia7/1_klasy.php
+++ zajęcia7/1_klasy.php
@@ -1,28 +1,71 @@
 <?php
+//set - jeśli chcemy coś ustawić
+//get - jeśli chcemy coś pobrać 
 
 class User
 {
     public $name; //właściwość
     public $surname; //właściwość
+    public $city; //właściwość
+    public $height; //właściwość
 
     public function setName($name) //metoda
     {
         // $this->name = 'xyz';
         $this->name = $name;
-    } 
+    }
+    
+    public function getName() //metoda
+    {
+        return $this->name;
+    }
+    public function setSurname($surname) //metoda
+    {
+        $this->surname = $surname;
+    }
+    
+    public function getSurname() //metoda
+    {
+        return $this->surname;
+    }
+
+    public function setData($name,$surname,$city,$height) //metoda
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->city = $city;
+        $this->height = $height;
+    }
+
+    public function getData()
+    {
+        echo <<<SHOW
+        <hr>Twoje dane:<br>
+        Imię: $this->name<br>
+        Nazwisko: $this->surname<br>
+        Miasto: $this->city<br>
+        Wzrost: $this->height<hr>
+        SHOW;
+    }
 }
 
-    $person1 = new User;
+    $person1 = new User();
     $person1->name = 'Janusz';
     $person1->surname = 'Nowak';
 
 
+    echo "Imię i nazwisko: ",$person1->name,' ',$person1->surname,'<br>'; //pobieranie właściwości dla obiektu person1
+
+
+    $person1->setName('Anna');
     echo "Imię i nazwisko: ",$person1->name,' ',$person1->surname,'<br>';
 
+    echo "Imię: ", $person1->getName()," ","Nazwisko: ",$person1->getSurname(); //wywoływanie metody getName dla obiektu person1
 
-    $person1->setName('Kowalski');
+    $person1->setData('Krystyna','Kowalska', 'Poznań', 168);
+    $person1->getData();
 
-    echo "Imię i nazwisko: ",$person1->name,' ',$person1->surname,'<br>';
-
-    
+    $person2 = new User();
+    $person2->setData('Paweł', 'Nowak', 'Gniezno', 200);
+    $person2->getData();
 ?>
---
+++ zajęcia7/ex/1_ex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h4>Formularz z danymi użytkownika</h4>
+        <form action="data.php" method="get">
+        <input type="text" name="name" placeholder="imię"><br><br>
+        <input type="text" name="surname" placeholder="Nazwisko"><br><br>
+        <input type="number" name="age" placeholder="Wiek"><br><br>
+        <input type="number" name="height" placeholder="Wzrost"><br><br>
+        <input type="text" name="city" placeholder="Miasto"><br><br>
+        <input type="submit" value="Wyślij dane">
+
+    </form>
+</body>
+
+</html>

fafee957df0980c48b8a799cae02949c46c6ff51
Lukomska <ola.lukomska11@wp.pl>
2021-02-25 13:58:44 +0100

    1

---
+++ zajęcia8/Metody_i_właściwości/2_klasa_final.php
@@ -0,0 +1,15 @@
+<?php
+
+final class Person{
+    ///metody
+}
+
+class Child extends Person{
+    //metody
+}
+
+//Fatal error: Class Child may not inherit from final class (Person) in C:\xampp\htdocs\GIT\zajęcia8\Metody_i_właściwości\2_klasa_final.php on line 9
+
+//Jeżeli przed klasą mamy final to nie może być klasy dziedziczącej po niej
+
+?>

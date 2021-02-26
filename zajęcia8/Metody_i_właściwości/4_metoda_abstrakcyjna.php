3a553b64abd3a92a33014823e9fe353da2b73942
Lukomska <ola.lukomska11@wp.pl>
2021-02-26 21:07:04 +0100

    ok

--- zajęcia8/Metody_i_właściwości/4_metoda_abstrakcyjna.php
+++ zajęcia8/Metody_i_właściwości/4_metoda_abstrakcyjna.php
@@ -1,5 +1,21 @@
 <?php
+abstract class Person{
+    abstract public function getName();
+}
+
+class Child{
+    public function getName(){
+        return "Imię dziecka";
+    }
+    public function go(){
+        return "Dziecko idzie";
+    }
+}
 
 
+// echo (new Child())->go();
+// echo Child::go(); musi byc metoda statyczna zeby to wywolac
+
+$obj = new Child();
 
 ?>
